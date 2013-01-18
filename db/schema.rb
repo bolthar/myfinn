10bfbcaf34f83eaf2c290b0a9a1ad467c26a2a1b
andrea <andrea@PICCOLO.(none)>
2013-01-18 17:32:16 +0100

    added status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130108135022) do
+ActiveRecord::Schema.define(:version => 20130118163115) do
 
   create_table "apartments", :force => true do |t|
     t.string   "title"
@@ -27,13 +27,14 @@ ActiveRecord::Schema.define(:version => 20130108135022) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "score"
     t.integer  "rating",           :default => 0
+    t.integer  "score"
     t.string   "location"
     t.float    "lat"
     t.float    "lng"
     t.boolean  "contacted",        :default => false
     t.boolean  "rejected",         :default => false
+    t.integer  "status",           :default => 0
   end
 
   create_table "apartments_features", :id => false, :force => true do |t|
